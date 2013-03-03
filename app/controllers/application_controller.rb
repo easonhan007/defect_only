204672a7985bf88a5e421e6f59a3a5c1749be238
easonhan <nbkhic@qq.com>
2013-03-03 19:59:20 +0800

    display current login user in layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,23 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter :authorize
-  
+
   protected 
 
   def authorize
-    unless User.find_by_id(session[:user_id])
-      redirect_to login_url, notice: 'Please log in'
+    @current_user = User.find_by_id(session[:user_id])
+
+    unless @current_user
+      redirect_to login_url, notice: 'Please log in' and return false
     end #unless
   end
+
+  def current_user
+    return unless session[:user_id]
+    @current_user
+  end
+
+  helper_method :current_user
+
 
 end
